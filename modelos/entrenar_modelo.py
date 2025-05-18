03c32c406224fade47182af823ab28d32c01470d
RodrigoCarbone <rncarbone@outlook.com>
2025-05-18 22:15:17 +0000

    modelo funcionando ok. se volvio al dataset original ya que al hacerle cambios no respondia bien

--- modelos/entrenar_modelo.py
+++ modelos/entrenar_modelo.py
@@ -1,15 +1,19 @@
 import pandas as pd
-import unicodedata
-import re
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 import joblib
+import re
+import unicodedata
 
 def limpiar_texto(texto):
+    # Pasar a minúsculas
     texto = texto.lower()
+    # Eliminar tildes
     texto = ''.join(c for c in unicodedata.normalize('NFD', texto)
                     if unicodedata.category(c) != 'Mn')
+    # Eliminar signos de puntuación
     texto = re.sub(r'[^\w\s]', '', texto)
+    # Eliminar espacios múltiples
     texto = re.sub(r'\s+', ' ', texto).strip()
     return texto
 
@@ -35,14 +39,18 @@ def obtener_respuesta(pregunta_usuario):
     probas = modelo.predict_proba(pregunta_vectorizada)[0]
     indice_max = probas.argmax()
     confianza = probas[indice_max]
+    
+    print("Cantidad de muestras en el dataset:", len(preguntas))
+    print("Cantidad de palabras en el vocabulario:", len(vectorizer.get_feature_names_out()))
+    print("Probabilidades de cada clase:", probas)
+    print("Confianza calculada:", confianza)
+    print("Categoría elegida:", modelo.classes_[indice_max])
 
     if confianza < 0.3:
-         with open("log_desconocidas.txt", "a") as f:
+        with open("log_desconocidas.txt", "a") as f:
             f.write(pregunta_usuario + "\n")
-    return "No entendi lo que me preguntaste, me lo podes repetir?"
-
-    respuesta = modelo.classes_[indice_max]
-    return respuesta
+        return "No entendi lo que me preguntaste, me lo podes repetir?"
+    return modelo.classes_[indice_max]
 
 while True:
     pregunta_usuario = input("Podes preguntarme algo o podes escribir 'salir': ")
